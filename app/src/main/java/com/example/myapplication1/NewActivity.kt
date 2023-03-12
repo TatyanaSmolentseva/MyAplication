bf04bc41f604d8854bf085c064e9487d2ce5f3e2
smol_td <smolentseva.1994@gmail.com>
2023-03-12 17:43:13 +0300

    extra

--- app/src/main/java/com/example/myapplication1/NewActivity.kt
+++ app/src/main/java/com/example/myapplication1/NewActivity.kt
@@ -1,27 +1,25 @@
 package com.example.myapplication1
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class NewActivity : AppCompatActivity() {
+    companion object {
+        const val INTENT_NAME = "text"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_new)
+        val extra = intent.getStringExtra(INTENT_NAME)
         val button = findViewById<Button>(R.id.button)
         val text = findViewById<TextView>(R.id.textView)
-        val editText = findViewById<EditText>(R.id.editText)
-        val intent = Intent(this, NewActivity::class.java)
-
+        text.text = extra
 
         button.setOnClickListener {
-            val resultText = editText.text.toString()
-            intent.putExtra(NewActivity.INTENT_NAME,resultText)
-            startActivity(intent)
+            text.text = "Пишите, Пишите"
         }
     }
 }
