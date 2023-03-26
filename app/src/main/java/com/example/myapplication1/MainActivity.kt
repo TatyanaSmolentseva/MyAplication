d03b2399a4a903bf6737a2006f668e2dcf556790
Smol_td <smolentseva.1994@gmail.com>
2023-03-26 18:22:12 +0300

    lesson_3

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication1
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -9,21 +11,32 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val file = "SharedPreference"
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
-        val button2 = findViewById<Button>(R.id.button2)
-        val intent = Intent(this, NewActivity::class.java)
+
+        val sharedPreferences = getSharedPreferences(file, Context.MODE_PRIVATE) //с 19-21 строку можно использовать в других классах//
+        val edit = sharedPreferences.edit()
+
+        val save = findViewById<Button>(R.id.Save)
+        val textView = findViewById<TextView>(R.id.textView)
+        val editText = findViewById<EditText>(R.id.editText)
+        val load = findViewById<Button>(R.id.Load)
 
 
-        button.setOnClickListener {
-            intent.putExtra(NewActivity.INTENT_NAME,"КНОПКА")
-            startActivity(intent)
+        save.setOnClickListener {
+
+            val text = editText.text.toString()
+            edit.putString("key", text).commit()
+
 
         }
-        button2.setOnClickListener {
-            intent.putExtra(NewActivity.INTENT_NAME,"КНОПКА2")
-            startActivity(intent)
-    }
+        load.setOnClickListener {
+            val pulOut = sharedPreferences.getString("key","лажа")
+            textView.text = pulOut
+
+        }
 }}
